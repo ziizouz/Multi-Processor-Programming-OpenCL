87286168246a7779a9f9e4dce20d256accdb140b
Dhiaa Zidane <dzidane18@student.oulu.fi>
2019-03-25 20:43:53 +0200

    Working implementation of devices information retrieving function

--- MPP_OPENCL/Main.cpp
+++ MPP_OPENCL/Main.cpp
@@ -13,154 +13,57 @@ using namespace std;
 using namespace cl;
 #define MAX_SOURCE_SIZE (0x100000)
 
+/* Funtions declarations */
+void getAvailableDevicesInfo();
+
 int main(void) {
-	printf("started running\n");
-	/*
-	// Create the two input vectors
-	int i;
-	const int LIST_SIZE = 1024;
-	int *A = (int*)malloc(sizeof(int)*LIST_SIZE);
-	int *B = (int*)malloc(sizeof(int)*LIST_SIZE);
-	for (i = 0; i < LIST_SIZE; i++) {
-		A[i] = i;
-		B[i] = LIST_SIZE - i;
-	}
-
-	// Load the kernel source code into the array source_str
-	FILE *fp;
-	char *source_str;
-	size_t source_size;
-
-	fp = fopen("vector_add_kernel.cl", "r");
-	if (!fp) {
-		fprintf(stderr, "Failed to load kernel.\n");
-		exit(1);
-	}
-	source_str = (char*)malloc(MAX_SOURCE_SIZE);
-	source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
-	fclose(fp);
-	printf("kernel loading done\n");
-	// Get platform and device information
-	cl_device_id device_id = NULL;
-	cl_uint ret_num_devices;
-	cl_uint ret_num_platforms;
-
-
-	cl_int ret = clGetPlatformIDs(0, NULL, &ret_num_platforms);
-	cl_platform_id *platforms = NULL;
-	platforms = (cl_platform_id*)malloc(ret_num_platforms * sizeof(cl_platform_id));
-
-	ret = clGetPlatformIDs(ret_num_platforms, platforms, NULL);
-	printf("ret at %d is %d\n", __LINE__, ret);
-
-	ret = clGetDeviceIDs(platforms[1], CL_DEVICE_TYPE_ALL, 1,
-		&device_id, &ret_num_devices);
-	printf("ret at %d is %d\n", __LINE__, ret);
-	// Create an OpenCL context
-	cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
-	printf("ret at %d is %d\n", __LINE__, ret);
-
-	// Create a command queue
-	cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
-	printf("ret at %d is %d\n", __LINE__, ret);
-
-	// Create memory buffers on the device for each vector 
-	cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
-		LIST_SIZE * sizeof(int), NULL, &ret);
-	cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
-		LIST_SIZE * sizeof(int), NULL, &ret);
-	cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
-		LIST_SIZE * sizeof(int), NULL, &ret);
-
-	// Copy the lists A and B to their respective memory buffers
-	ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
-		LIST_SIZE * sizeof(int), A, 0, NULL, NULL);
-	printf("ret at %d is %d\n", __LINE__, ret);
-
-	ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0,
-		LIST_SIZE * sizeof(int), B, 0, NULL, NULL);
-	printf("ret at %d is %d\n", __LINE__, ret);
-
-	printf("before building\n");
-	// Create a program from the kernel source
-	cl_program program = clCreateProgramWithSource(context, 1,
-		(const char **)&source_str, (const size_t *)&source_size, &ret);
-	printf("ret at %d is %d\n", __LINE__, ret);
-
-	// Build the program
-	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
-	printf("ret at %d is %d\n", __LINE__, ret);
-
-	printf("after building\n");
-	// Create the OpenCL kernel
-	cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
-	printf("ret at %d is %d\n", __LINE__, ret);
-
-	// Set the arguments of the kernel
-	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
-	printf("ret at %d is %d\n", __LINE__, ret);
-
-	ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
-	printf("ret at %d is %d\n", __LINE__, ret);
-
-	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
-	printf("ret at %d is %d\n", __LINE__, ret);
-
-	//added this to fix garbage output problem
-	//ret = clSetKernelArg(kernel, 3, sizeof(int), &LIST_SIZE);
-
-	printf("before execution\n");
-	// Execute the OpenCL kernel on the list
-	size_t global_item_size = LIST_SIZE; // Process the entire lists
-	size_t local_item_size = 64; // Divide work items into groups of 64
-	ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL,
-		&global_item_size, &local_item_size, 0, NULL, NULL);
-	printf("after execution\n");
-	// Read the memory buffer C on the device to the local variable C
-	int *C = (int*)malloc(sizeof(int)*LIST_SIZE);
-	ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0,
-		LIST_SIZE * sizeof(int), C, 0, NULL, NULL);
-	printf("after copying\n");
-	// Display the result to the screen
-	for (i = 0; i < LIST_SIZE; i++)
-		printf("%d + %d = %d\n", A[i], B[i], C[i]);
-
-	// Clean up
-	ret = clFlush(command_queue);
-	ret = clFinish(command_queue);
-	ret = clReleaseKernel(kernel);
-	ret = clReleaseProgram(program);
-	ret = clReleaseMemObject(a_mem_obj);
-	ret = clReleaseMemObject(b_mem_obj);
-	ret = clReleaseMemObject(c_mem_obj);
-	ret = clReleaseCommandQueue(command_queue);
-	ret = clReleaseContext(context);
-	free(A);
-	free(B);
-	free(C);
-	*/
+	cout << "Program starting \n" << endl;
 	
+	// Priting out all information of available devices
+	cout << "Gettings devices information:\n" << endl;
+	getAvailableDevicesInfo();
+
+
+
+	system("pause");
+	return 0;
+}
+
+// Devices informations
+void getAvailableDevicesInfo() 
+{
+	// Gettings all available platforms
 	vector<cl::Platform> platforms;
 	Platform::get(&platforms);
 
 	_ASSERT(platforms.size() > 0);
 
 	vector <Device> devices;
-	
+
 	for (auto platform : platforms)
 	{
-		platform.getDevices(CL_DEVICE_TYPE_ALL, &devices); // Get all platform devices
+		// Get all platform devices 
+		platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
 
-		cout << "Platform name: ";
+		cout << "* Platform name: ";
 		cout << platform.getInfo<CL_PLATFORM_NAME>() << endl;
 
 		_ASSERT(devices.size() > 0);
 
 		cout << "Platform devices:" << endl;
-		for (auto device : devices) // first device
-			cout << device.getInfo<CL_DEVICE_VENDOR>() << endl;
+		for (auto device : devices)
+		{
+			cout << "Device name: " << device.getInfo<CL_DEVICE_NAME>() << endl;
+			cout << "Device vendor: " << device.getInfo<CL_DEVICE_VENDOR>() << endl;
+			cout << "Device local memory type: " << device.getInfo<CL_DEVICE_LOCAL_MEM_TYPE>() << endl;
+			cout << "Device local memory size: " << device.getInfo<CL_DEVICE_LOCAL_MEM_SIZE>() << endl;
+			cout << "Device max frequency: " << device.getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>() << endl;
+			cout << "Device max constant buffer size: " << device.getInfo<CL_DEVICE_MAX_CONSTANT_BUFFER_SIZE>() << endl;
+			cout << "Device max work group size: " << device.getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>() << endl;
+			cout << "Device max work item size: " << device.getInfo<CL_DEVICE_MAX_WORK_ITEM_SIZES>()[0] << endl;
+			cout << endl;
+		}
+		cout << endl;
 	}
 
-	system("pause");
-	return 0;
 }
